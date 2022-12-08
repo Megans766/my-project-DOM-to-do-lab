7ecf325a244d2d54c69b6472ddad6d8198e2a703
Megan Smith <megans766@gmail.com>
2022-12-07 20:05:25 -0500

    completed 5&6

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
 // console.log("the check");
 
+//cached reference elements below
+const inputElement = document.querySelector('input')
+const buttonElement = document.querySelector('button')
+const ulElement = document.querySelector('ul')
+
+
+buttonElement.addEventListener('click', function(buttonEvent) {
+    // console.log(buttonEvent);
+    let newList = document.createElement('li')
+    // console.log(newList);
+    newList.textContent = inputElement.value
+    // console.log(newList);
+    inputElement.value = ''
+    ulElement.appendChild(newList)
+    // console.log(newList);
+})
